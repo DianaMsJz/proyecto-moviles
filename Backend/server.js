f474a605d1af0cfdb8f5a21e14f824f6e6adab5e
IramRobledo <iramrobledo.25@gmail.com>
2021-11-09 17:47:43 -0700

    Configuración de servidor con nodejs y rutas

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+//CORS es un paquete node.js para proporcionar un middleware (agente intermedio) Connect / Express que se puede usar 
+//para habilitar CORS con varias opciones.
+//Morgan: para debuggear los posibles errores que aparezcan después de crear la API
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev')); //para debuggear errores
+app.use(express.json()); //Para parsear las respuestas recibidas en .json
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.set('port', port);
+app.disable('x-powered-by'); //seguridad
+
+server.listen(3000, '192.168.56.1' || 'localhost', function() {
+    console.log('Aplicación con NodeJS ' + port + ' Iniciada...')
+});
+
+app.get('/', (req, res) => { //para obtener datos
+    res.send('Ruta raíz del backend');
+});
+
+app.get('/test', (req, res) => { //para obtener datos
+    res.send('Esta es la ruta TEST');
+});
+
+//MANEJO DE ERRORES
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
